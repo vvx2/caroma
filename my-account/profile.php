84e167a92e9077c5065b2c64d997a10e5d64b6d0
weex2 <puahweewee@gmail.com>
2020-11-28 20:01:14 +0800

    profile edit change email reset passwod

--- my-account/profile.php
+++ my-account/profile.php
@@ -7,11 +7,33 @@ if ($login != 1) {
 	exit();
 }
 $tb = "users u left join user_address ua on u.id = ua.user_id";
-$col = "u.name as name, u.email as email, ua.contact as contact, ua.address as address, ua.postcode as postcode, ua.city as city, ua.state as state";
+$col = "u.name as name,u.type as user_type,u.image as image, u.email as email, ua.contact as contact, ua.address as address, ua.postcode as postcode, ua.city as city, ua.state as state";
 $opt = 'u.id = ?';
 $arr = array($user_id);
 $result_user = $db->advwhere($col, $tb, $opt, $arr);
 $result_user = $result_user[0];
+
+$user_img = $result_user['image'];
+$user_type_display = "";
+if ($result_user['user_type'] == 1) {
+	$user_type_display = "NORMAL USER";
+} else if ($result_user['user_type'] == 2) {
+	$user_type_display = "DISTRIBUTOR";
+} else if ($result_user['user_type'] == 3) {
+	$user_type_display = "DEALER";
+}
+
+if ($user_img != "" || $user_img != NULL) {
+	$image_path = "../img/profile/" . $user_img;
+} else {
+	$image_path = "dist/img/user1.png";
+}
+
+$user_order = $db->where("id", "orders", "users_id", $user_id);
+$user_cart = $db->where("id", "cart", "customer_id", $user_id);
+
+$count_order = count($user_order);
+$count_cart = count($user_cart);
 
 ?>
 
@@ -59,9 +81,9 @@ $result_user = $result_user[0];
 									<div class="profile-box">
 										<div class="profile-info-wrap text-center">
 											<div class="profile-info pt-40">
-												<img class="img-circle inline-block mt-40 mb-10" src="dist/img/user1.png" alt="user" />
-												<h4 class="txt-light block  mb-5 capitalize-font">willard bryant</h4>
-												<h6 class="txt-light block uppercase-font pb-40">Distributor</h6>
+												<img class="img-circle inline-block mt-40 mb-10" src="<?php echo $image_path; ?>" alt="user" />
+												<h4 class="txt-light block  mb-5 capitalize-font"><?php echo $result_user['name']; ?></h4>
+												<h6 class="txt-light block uppercase-font pb-40"><?php echo $user_type_display; ?></h6>
 											</div>
 											<div class="profile-image-overlay"></div>
 										</div>
@@ -69,7 +91,7 @@ $result_user = $result_user[0];
 											<div class="row">
 												<div class="col-sm-4 text-center">
 													<i class="fa fa-truck block mb-10"></i>
-													<span class="counts block head-font mb-5">10</span>
+													<span class="counts block head-font mb-5"><?php echo $count_order; ?></span>
 													<span class="counts-text block">Total Order</span>
 												</div>
 												<div class="col-sm-4 text-center">
@@ -79,7 +101,7 @@ $result_user = $result_user[0];
 												</div>
 												<div class="col-sm-4 text-center">
 													<i class="fa fa-shopping-cart block mb-10"></i>
-													<span class="counts block head-font mb-5">1</span>
+													<span class="counts block head-font mb-5"><?php echo $count_cart; ?></span>
 													<span class="counts-text block">Cart Items</span>
 												</div>
 											</div>
@@ -104,17 +126,18 @@ $result_user = $result_user[0];
 												<div class="row">
 													<div class="col-md-12">
 														<div class="form-wrap">
-															<form data-toggle="validator" role="form">
+															<form data-toggle="validator" role="form" id="form_profile" action="api/user_sql.php?type=profile_edit&tb=user" method="post" enctype="multipart/form-data">
+																<input type="hidden" name="token" value="<?php echo $token; ?>" />
 																<div class="form-group">
 																	<label for="inputName" class="control-label mb-10">Full Name</label>
-																	<input type="text" class="form-control" id="inputName" placeholder="Cina Saffary" data-error="Name Is Required" value="<?php echo $result_user['name']; ?>" required>
+																	<input type="text" class="form-control" id="inputName" placeholder="Cina Saffary" data-error="Name Is Required" name="name" value="<?php echo $result_user['name']; ?>" required>
 																	<div class="help-block with-errors"></div>
 																</div>
 																<div class="form-group has-feedback">
 																	<label for="inputPhone" class="control-label mb-10">Contact Number</label>
 																	<div class="input-group">
 																		<span class="input-group-addon">+6</span>
-																		<input type="text" pattern="^(\+?6?01)[0|1|2|3|4|6|7|8|9]-*[0-9]{7,8}$" maxlength="11" class="form-control" value="<?php echo $result_user['contact']; ?>" id="inputPhone" placeholder="012 3456 789" data-error="Eg : 016 2345 678" required>
+																		<input type="text" pattern="^(\+?6?01)[0|1|2|3|4|6|7|8|9]-*[0-9]{7,8}$" maxlength="11" class="form-control" name="contact" value="<?php echo $result_user['contact']; ?>" id="inputPhone" placeholder="012 3456 789" data-error="Eg : 016 2345 678" required>
 																		<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
 																	</div>
 
@@ -122,7 +145,7 @@ $result_user = $result_user[0];
 																</div>
 																<div class="form-group">
 																	<label for="inputAddress" class="control-label mb-10">Address</label>
-																	<input type="text" class="form-control" id="inputAddress" placeholder="No 321, Lorong Sentosa, Tmn Lgd 15, 81100 Johor Bahru, Johor" value="<?php echo $result_user['address']; ?>" data-error="Address Is Required" required>
+																	<input type="text" class="form-control" id="inputAddress" placeholder="No 321, Lorong Sentosa, Tmn Lgd 15, 81100 Johor Bahru, Johor" name="address" value="<?php echo $result_user['address']; ?>" data-error="Address Is Required" required>
 																	<div class="help-block with-errors"></div>
 																</div>
 
@@ -147,19 +170,18 @@ $result_user = $result_user[0];
 																				<?php
 																				} ?>
 																			</select>
-																			<!-- <input type="text" class="form-control" id="inputStates" placeholder="Johor" value="<?php echo $result_user['state']; ?>" data-error="States Is Required" required> -->
 																			<div class="help-block with-errors"></div>
 																		</div>
 
 																		<div class="col-sm-4 col-12">
 																			<label for="inputCity" class="control-label mb-10">City</label>
-																			<input type="text" class="form-control" id="inputCity" placeholder="Johor Bahru" value="<?php echo $result_user['city']; ?>" data-error="City Is Required" required>
+																			<input type="text" class="form-control" id="inputCity" placeholder="Johor Bahru" name="city" value="<?php echo $result_user['city']; ?>" data-error="City Is Required" required>
 																			<div class="help-block with-errors"></div>
 																		</div>
 
 																		<div class="col-sm-4 col-12">
 																			<label for="inputZip" class="control-label mb-10">Zip Code</label>
-																			<input type="text" class="form-control" id="inputZip" placeholder="81100" value="<?php echo $result_user['postcode']; ?>" data-error="Zip Code Is Required" required>
+																			<input type="text" class="form-control" id="inputZip" placeholder="81100" name="postcode" value="<?php echo $result_user['postcode']; ?>" data-error="Zip Code Is Required" required>
 																			<div class="help-block with-errors"></div>
 																		</div>
 																	</div>
@@ -167,12 +189,12 @@ $result_user = $result_user[0];
 
 																<div class="form-group">
 																	<label for="inputAddress" class="control-label mb-10">Profile Picture</label>
-																	<input type="file" class="form-control" id="inputAddress" accept="image/x-png,image/gif,image/jpeg" data-error="Only for .jpg .jpeg .png" >
+																	<input type="file" class="form-control" id="inputAddress" name="img" accept="image/x-png,image/gif,image/jpeg" data-error="Only for .jpg .jpeg .png">
 																	<div class="help-block with-errors"></div>
 																</div>
 
 																<div class="form-group mb-0">
-																	<button type="submit" class="btn btn-success btn-anim"><i class="icon-rocket"></i><span class="btn-text">Submit</span></button>
+																	<button type="submit" class="btn btn-success btn-anim" name="btnAction"><i class="icon-rocket"></i><span class="btn-text">Submit</span></button>
 																</div>
 															</form>
 														</div>
@@ -184,31 +206,32 @@ $result_user = $result_user[0];
 												<div class="row">
 													<div class="col-md-12">
 														<div class="form-wrap">
-															<form data-toggle="validator" role="form">
+															<form data-toggle="validator" role="form" id="form_password" action="api/user_sql.php?type=change_password&tb=user" method="post" enctype="multipart/form-data">
+																<input type="hidden" name="token" value="<?php echo $token; ?>" />
 																<div class="form-group">
 																	<div class="form-group col-sm-12 no-padding">
 																		<label for="inputPassword" class="control-label mb-10">Old Password</label>
-																		<input data-match-error="Old Password Is Required" type="password" class="form-control" id="inputPassword" placeholder="Password" required>
+																		<input data-match-error="Old Password Is Required" name="old_password" type="password" class="form-control" id="inputPassword" placeholder="Password" required>
 																	</div>
 																</div>
 
 																<div class="form-group">
 																	<div class="row">
 																		<div class="form-group col-sm-12">
-																			<label for="inputPassword" class="control-label mb-10">New Password</label>
-																			<input type="password" data-minlength="8" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" class="form-control" id="inputPassword" placeholder="Password" required>
+																			<label for="inputnewPassword" class="control-label mb-10">New Password</label>
+																			<input type="password" data-minlength="8" name="new_password" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" class="form-control" id="inputnewPassword" placeholder="Password" required>
 																			<div class="help-block">UpperCase, LowerCase, Number, SpecialChar and min 8 Chars</div>
 																		</div>
 																		<div class="form-group col-sm-12">
-																			<label for="inputPassword" class="control-label mb-10">Confirm Password</label>
-																			<input type="password" class="form-control" id="inputPasswordConfirm" data-match="#inputPassword" data-match-error="Whoops, these don't match" placeholder="Confirm" required>
+																			<label for="inputPasswordConfirm" class="control-label mb-10">Confirm Password</label>
+																			<input type="password" class="form-control" name="confirm_password" id="inputPasswordConfirm" data-match="#inputnewPassword" data-match-error="Whoops, these don't match" placeholder="Confirm" required>
 																			<div class="help-block with-errors"></div>
 																		</div>
 																	</div>
 																</div>
 
 																<div class="form-group mb-0">
-																	<button type="submit" class="btn btn-success btn-anim"><i class="icon-rocket"></i><span class="btn-text">Submit</span></button>
+																	<button type="submit" class="btn btn-success btn-anim" name="btnAction"><i class="icon-rocket"></i><span class="btn-text">Submit</span></button>
 																</div>
 															</form>
 														</div>
@@ -221,16 +244,17 @@ $result_user = $result_user[0];
 												<div class="row">
 													<div class="col-md-12">
 														<div class="form-wrap">
-															<form data-toggle="validator" role="form">
+															<form data-toggle="validator" role="form" id="form_email" action="api/user_sql.php?type=change_email&tb=user" method="post" enctype="multipart/form-data">
+																<input type="hidden" name="token" value="<?php echo $token; ?>" />
 																<div class="form-group">
 																	<div class="form-group col-sm-12 no-padding">
 																		<label for="inputEmail" class="control-label mb-10">Email</label>
-																		<input data-match-error="Email Is Required" type="email" class="form-control" id="inputEmail" value="<?php echo $result_user['email']; ?>" placeholder="Email" required>
+																		<input data-match-error="Email Is Required" type="email" class="form-control" id="inputEmail" name="email" value="<?php echo $result_user['email']; ?>" placeholder="Email" required>
 																	</div>
 																</div>
 
 																<div class="form-group mb-0">
-																	<button type="submit" class="btn btn-success btn-anim"><i class="icon-rocket"></i><span class="btn-text">Submit</span></button>
+																	<button type="submit" class="btn btn-success btn-anim" name="btnAction"><i class="icon-rocket"></i><span class="btn-text">Submit</span></button>
 																</div>
 															</form>
 														</div>
