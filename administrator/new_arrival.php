a8712da9d9b10379465030ce48b01e6725f4fb29
puah <puahweewee@gmail.com>
2021-01-09 18:04:51 +0800

    admin add new arrival

---
+++ administrator/new_arrival.php
@@ -0,0 +1,238 @@
+<?php
+require_once('inc/init.php');
+$PageName = "promotion";
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php include_once('inc/header.php'); ?>
+    <link href="css/plugins/iCheck/custom.css" rel="stylesheet">
+    <link href="css/plugins/awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css" rel="stylesheet">
+    <link href="css/plugins/datapicker/datepicker3.css" rel="stylesheet">
+    <link href="css/plugins/touchspin/jquery.bootstrap-touchspin.min.css" rel="stylesheet">
+    <link href="css/plugins/dualListbox/bootstrap-duallistbox.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="wrapper">
+        <!-- left nav -->
+        <?php include_once('inc/admin_nav.php'); ?>
+        <!-- left nav -->
+        <div id="page-wrapper" class="gray-bg">
+            <!-- top nav -->
+            <?php include_once('inc/top_nav.php'); ?>
+            <!-- top nav -->
+            <div class="wrapper wrapper-content wrapperes">
+                <div class="col-lg-12" style="padding-left:0px ; padding-right :0px ; padding-bottom : 20px">
+                    <div class="row">
+
+                        <div class="col-md-9">
+                        </div>
+                        <div class="col-md-3 text-center product_btns">
+                            <a data-toggle="modal" class="btn btn-primary btn-lg btn-block" href="#add_promotion"> &nbsp;Update New Arrival</a>
+
+                        </div>
+                    </div>
+
+                </div>
+                <!-- ------------------------------------------- -->
+                <!--            Modal To Add New Arrival         -->
+                <!-- ------------------------------------------- -->
+                <div class="modal inmodal" id="add_promotion" tabindex="-1" role="dialog" aria-hidden="true">
+                    <div class="modal-dialog modal-lg">
+                        <div class="modal-content animated fadeIn">
+                            <form role="form" id="form_promotion" action="new_arrival_generate.php?type=add_new_arrival&tb=admin" method="post" enctype="multipart/form-data">
+                                <input type="hidden" name="token" value="<?php echo $token; ?>" />
+
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+
+                                    <h4 class="modal-title">Add New Arrival</h4>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="form-group">
+                                        <label class="font-normal">Product for new arrival - <span class="text-success">New arrival product</span></label>
+                                        <select class="form-control dual_select " name="new_arrival[]" multiple required>
+                                            <?php
+
+                                            $tb = "product left join product_translation on product.id = product_translation.product_id";
+                                            $col = "product.id as id, product_translation.name as name";
+                                            $opt = 'status = ? && product_translation.language = ?';
+                                            $arr = array(1, "en");
+                                            $result = $db->advwhere($col, $tb, $opt, $arr);
+                                            foreach ($result as $product) {
+
+                                                $col = "*";
+                                                $table = "new_arrival";
+                                                $opt = 'product_id = ?';
+                                                $arr = array($product['id']);
+                                                $new_arrival_product = $db->advwhere($col, $table, $opt, $arr);
+
+                                            ?>
+                                                <option value="<?php echo $product['id']; ?>" <?php echo (count($new_arrival_product) != 0) ? "selected" : ""; ?>><?php echo $product['name']; ?></option>
+                                            <?php
+                                            } ?>
+                                        </select>
+                                    </div>
+
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-white" data-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary" name="btnAction"><strong>Confirm</strong></button>
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <!-- ------------------------------------------- -->
+                <!--            Modal To Add New Arrival         -->
+                <!-- ------------------------------------------- -->
+
+                <div class="col-lg-12">
+                    <div class="ibox ">
+                        <div class="ibox-title">
+                            <h5>New Arrival</h5>
+
+                            <div class="ibox-tools">
+                                <a class="collapse-link">
+                                    <i class="fa fa-chevron-up"></i>
+                                </a>
+                                <a class="close-link">
+                                    <i class="fa fa-times"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="ibox-content">
+
+                            <table class="table table-striped table-bordered table-hover dataTables-example">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Name</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $i = 1;
+                                    $tb = "new_arrival left join product on new_arrival.product_id =  product.id left join product_translation on product.id = product_translation.product_id left join category_translation on product.category = category_translation.category_id";
+                                    $col = "*,product.id as product_id, product_translation.description as product_description, product_translation.name as product_name, category_translation.name as category_name";
+                                    $opt = 'product_translation.language = ? && category_translation.language = ? ORDER BY date_modified DESC';
+                                    $arr = array("en", "en");
+                                    $product = $db->advwhere($col, $tb, $opt, $arr);
+                                    foreach ($product as $new) {
+
+                                    ?>
+                                        <tr class="gradeX">
+                                            <td><?php echo $i; ?></td>
+                                            <td><?php echo $new["product_name"]; ?></td>
+                                        </tr>
+                                    <?php
+                                        $i++;
+                                    }
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+
+
+    </div>
+
+    </div>
+
+    <!-- this is for display modal by ajax -->
+    <div class="modal inmodal" id="myModal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content animated fadeIn">
+
+            </div>
+        </div>
+    </div>
+
+    <!-- Mainly scripts -->
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="js/inspinia.js"></script>
+    <script src="js/plugins/pace/pace.min.js"></script>
+
+    <!-- jQuery UI -->
+    <script src="js/plugins/jquery-ui/jquery-ui.min.js"></script>
+
+    <script src="js/plugins/dataTables/datatables.min.js"></script>
+    <script src="js/plugins/dataTables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Jquery Validate -->
+    <script src="js/plugins/validate/jquery.validate.min.js"></script>
+    <!-- Chosen -->
+    <script src="js/plugins/chosen/chosen.jquery.js"></script>
+    <!-- iCheck -->
+    <script src="js/plugins/iCheck/icheck.min.js"></script>
+
+    <!-- TouchSpin -->
+    <script src="js/plugins/touchspin/jquery.bootstrap-touchspin.min.js"></script>
+
+    <!-- Data picker -->
+    <script src="js/plugins/datapicker/bootstrap-datepicker.js"></script>
+
+    <!-- Dual Listbox -->
+    <script src="js/plugins/dualListbox/jquery.bootstrap-duallistbox.js"></script>
+
+    <!-- Page-Level Scripts -->
+    <script>
+        //this script for modal 
+        $('body').on('click', '[data-toggle="modal"]', function() {
+            $($(this).data("target") + ' .modal-content').load($(this).data("remote"));
+        });
+
+        $.validator.setDefaults({
+            ignore: ":hidden:not(.dual_select)"
+        }) //for all select having class .dual_select
+        $(document).ready(function() {
+
+            $('.dataTables-example').DataTable({
+                pageLength: 25,
+                responsive: true,
+                dom: '<"html5buttons"B>lTfgitp',
+                buttons: [
+
+                    {
+                        extend: 'csv',
+                    },
+                    {
+                        extend: 'excel',
+                        title: 'Product',
+                    }
+                ],
+
+            });
+
+            jQuery.validator.addMethod("noSpace", function(value, element) {
+                return value.indexOf(" ") < 0 && value != "";
+            }, "No space please and don't leave it empty");
+
+
+            $('.dual_select').bootstrapDualListbox({
+                selectorMinimalHeight: 160
+            });
+
+
+        });
+    </script>
+
+
+</body>
+
+</html>
