52bc1a9eaf666f253e453c33b3236a56d072e644
puah <puahweewee@gmail.com>
2021-01-10 17:06:33 +0800

    product display

--- shop.php
+++ shop.php
@@ -22,22 +22,43 @@
         $admin_id = $under_distributor;
 
 
-        $filter_table = "";
-        $filter_opt = " ";
-        $filter_arr = array($admin_id, $language, $user_type, $language, 1);
-
+        if (isset($_REQUEST["category"]) && $_REQUEST["category"] != 0) {
+            $filter_table = " left join category c on p.category = c.id";
+            $filter_opt = " && c.status =? && p.category =? ";
+            $filter_arr = array($admin_id, $language, $user_type, $language, 1, 1, $_REQUEST["category"]);
+        } else if ((isset($_REQUEST["price_from"]) && isset($_REQUEST["price_to"])) && ($_REQUEST["price_from"] != 0 && $_REQUEST["price_to"] != 0)) {
+            $filter_table = "";
+            $filter_opt = " && pp.price BETWEEN ? AND ? ";
+            $filter_arr = array($admin_id, $language, $user_type, $language, 1, $_REQUEST["price_from"], $_REQUEST["price_to"]);
+        } else {
+            $filter_table = "";
+            $filter_opt = " ";
+            $filter_arr = array($admin_id, $language, $user_type, $language, 1);
+        }
 
         $col = "*,dp.stock as dis_stock, p.stock as admin_stock, p.id as p_id, pt.name as pt_name, pt.description as pt_description, ct.name as ct_name, rate.rating as rating";
         $tb = "distributor_product dp left join product p on dp.product_id = p.id left join product_translation pt on p.id = pt.product_id left join product_role_price pp on p.id = pp.product_id left join category_translation ct on p.category = ct.category_id left join (SELECT product_id, (sum(rate) / count(product_id)) as rating FROM order_items where rate != 0 group by product_id) rate on p.id = rate.product_id " . $filter_table;
-        $opt = 'dp.user_id = ? && pt.language = ? && pp.type =? && ct.language =? && dp.status =?';
+        $opt = 'dp.user_id = ? && pt.language = ? && pp.type =? && ct.language =? && dp.status =?' . $filter_opt;
         $arr = $filter_arr;
         $count_product_result = $db->advwhere($col, $tb, $opt, $arr);
     } else {
 
-        $filter_table = "";
-        $filter_opt = " ";
-        $filter_arr = array($language, $user_type, $language, 1);
-
+        if (isset($_REQUEST["category"]) && $_REQUEST["category"] != 0) {
+            $filter_table = " left join category c on p.category = c.id";
+            $filter_opt = " && c.status =? && p.category =? ";
+            $filter_arr = array($language, $user_type, $language, 1, 1, $_REQUEST["category"]);
+            $check_sql = "category";
+        } else if ((isset($_REQUEST["price_from"]) && isset($_REQUEST["price_to"])) && ($_REQUEST["price_to"] != 0)) {
+            $filter_table = "";
+            $filter_opt = " && pp.price BETWEEN ? AND ? ";
+            $filter_arr = array($language, $user_type, $language, 1, $_REQUEST["price_from"], $_REQUEST["price_to"]);
+            $check_sql = "price";
+        } else {
+            $filter_table = "";
+            $filter_opt = " ";
+            $filter_arr = array($language, $user_type, $language, 1);
+            $check_sql = "none";
+        }
 
         $col = "*, p.id as p_id, pt.name as pt_name, pt.description as pt_description, ct.name as ct_name, rate.rating as rating";
         if (isset($_REQUEST["new_arrival"]) && $_REQUEST["new_arrival"] == 1) {
@@ -45,7 +66,7 @@
         } else {
             $tb = " product p left join product_translation pt on p.id = pt.product_id left join product_role_price pp on p.id = pp.product_id left join category_translation ct on p.category = ct.category_id left join (SELECT product_id, (sum(rate) / count(product_id)) as rating FROM order_items where rate != 0 group by product_id) rate on p.id = rate.product_id " . $filter_table;
         }
-        $opt = 'pt.language = ? && pp.type =? && ct.language =? && p.status =?';
+        $opt = 'pt.language = ? && pp.type =? && ct.language =? && p.status =?' . $filter_opt;
         $arr = $filter_arr;
         $count_product_result = $db->advwhere($col, $tb, $opt, $arr);
     }
@@ -261,7 +282,12 @@
                             <div class="wgt-content">
 
                                 <ul class="cat-list">
+
                                     <li class="cat-list-item"><a href="shop.php" class="cat-link">ALL</a></li>
+                                    <li class="cat-list-item"><a href="shop.php?new_arrival=1" class="cat-link">NEW ARRIVAL</a></li>
+                                    <?php if ($user_type == 1) { ?>
+                                        <li class="cat-list-item"><a href="shop.php?is_promotion=1" class="cat-link">PROMOTION</a></li>
+                                    <?php } ?>
                                     <?php
                                     $col = "c.id as c_id, ct.name as ct_name";
                                     $tb = "category c left join category_translation ct on c.id = ct.category_id";
@@ -275,10 +301,7 @@
                                     ?>
                                         <li class="cat-list-item"><a href="shop.php?category=<?php echo $cate['c_id']; ?>" class="cat-link"><?php echo $cate['ct_name']; ?></a></li>
                                     <?php } ?>
-                                    <li class="cat-list-item"><a href="shop.php?new_arrival=1" class="cat-link">NEW ARRIVAL</a></li>
-                                    <?php if ($user_type == 1) { ?>
-                                        <li class="cat-list-item"><a href="shop.php?is_promotion=1" class="cat-link">PROMOTION</a></li>
-                                    <?php } ?>
+
                                 </ul>
                             </div>
                         </div>
