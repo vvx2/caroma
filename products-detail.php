16b141cba4522e00e7e6b5661f7159bab4f5de2b
nickyz6 <nickyz111406@gmail.com>
2021-01-11 11:17:03 +0800

    solve primary display problem

--- products-detail.php
+++ products-detail.php
@@ -155,19 +155,49 @@
                         <ul class="biolife-carousel slider-for" data-slick='{"arrows":false,"dots":false,"slidesMargin":30,"slidesToShow":1,"slidesToScroll":1,"fade":true,"asNavFor":".slider-nav"}'>
                             <?php
                             $col = "*";
+                            $tb = "product";
+                            $opt = 'product_id = ?';
+                            $arr = array($product_id);
+                            $product_main_image = $db->advwhere($col, $tb, $opt, $arr);
+                            $product_main_image = $product_main_image[0];
+                            ?>
+                            <li><img src="img/product/<?php echo $product_main_image['image']; ?>" alt="" width="400" height="400"></li>
+                            <?php
+                            $col = "*";
                             $tb = "product_image";
                             $opt = 'product_id = ?';
                             $arr = array($product_id);
                             $product_image = $db->advwhere($col, $tb, $opt, $arr);
 
                             foreach ($product_image as $img) {
+                                if ($product_main_image['image'] == $img['image']) {
+                                    continue;
+                                }
                             ?>
                                 <li><img src="img/product/<?php echo $img['image']; ?>" alt="" width="400" height="400"></li>
                             <?php } ?>
                         </ul>
                         <ul class="biolife-carousel slider-nav" data-slick='{"arrows":false,"dots":false,"centerMode":false,"focusOnSelect":true,"slidesMargin":10,"slidesToShow":4,"slidesToScroll":1,"asNavFor":".slider-for"}'>
                             <?php
+                            $col = "*";
+                            $tb = "product";
+                            $opt = 'product_id = ?';
+                            $arr = array($product_id);
+                            $product_main_image = $db->advwhere($col, $tb, $opt, $arr);
+                            $product_main_image = $product_main_image[0];
+                            ?>
+                            <li><img src="img/product/<?php echo $product_main_image['image']; ?>" alt="" width="400" height="400"></li>
+                            <?php
+                            $col = "*";
+                            $tb = "product_image";
+                            $opt = 'product_id = ?';
+                            $arr = array($product_id);
+                            $product_image = $db->advwhere($col, $tb, $opt, $arr);
+
                             foreach ($product_image as $img) {
+                                if ($product_main_image['image'] == $img['image']) {
+                                    continue;
+                                }
                             ?>
                                 <li><img src="img/product/<?php echo $img['image']; ?>" alt="" width="88" height="88"></li>
                             <?php } ?>
