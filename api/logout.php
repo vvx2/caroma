4b284ec1666d41263db339a02de9b2ae3044d879
nickyz6 <nickyz111406@gmail.com>
2021-02-28 01:51:23 +0800

    logout debug for session language

--- api/logout.php
+++ api/logout.php
@@ -1,9 +1,17 @@
 <?php
 session_start();
+if (isset($_SESSION['language'])) {
+    $language = $_SESSION['language'];
+} else {
+    $_SESSION['language'] = "en";
+    $language = $_SESSION['language'];
+}
 session_destroy();
 unset($_SESSION["id"]);
 unset($_SESSION["admin"]);
+session_start();
 $login = 0;
 $_SESSION['login'] = $login;
+$_SESSION['language'] =  $language;
 header("Location: ../index.php");
 exit();
