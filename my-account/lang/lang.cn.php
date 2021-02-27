3e28dd91048da39346dd34ac2903afb9ca643ee1
weex2 <puahweewee@gmail.com>
2021-02-27 16:28:23 +0800

    test language

--- my-account/lang/lang.cn.php
+++ my-account/lang/lang.cn.php
@@ -1,19 +1,7 @@
 <?php
 
 //---------------------------
-//      login.php
+//      index.php
 //---------------------------
-$lang['lang-home'] = '首页';
-$lang['lang-authentication'] = '认证';
-$lang['lang-email_address']  = 'Email Address';
-$lang['lang-sign_in'] = 'Sign In';
-$lang['lang-forgot_password'] = 'Forgot your password';
-$lang['lang-new_customer'] = 'New Customer';
-$lang['lang-create_account'] = 'Create an account with us and you’ll be able to';
-$lang['lang-check_out_faser'] = 'Check out faster';
-$lang['lang-access_history'] = 'Access your order history';
-$lang['lang-track_new_orders'] = 'Track new orders';
-$lang['lang-save_items'] = 'Save items to your Wishlist';
-$lang['lang-craete_account'] = 'Create an account';
-$lang['lang-join_dealer'] = 'Join with Dealer';
+$lang['lang-dashboard'] = '仪表板';
 //---------------------------
