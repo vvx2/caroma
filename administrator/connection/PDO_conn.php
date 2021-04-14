2ca2aa7edf5980644bbdcfdda1cf66daf9fde6fa
puahvvx2 <puahweewee@gmail.com>
2021-04-14 23:42:19 +0800

    update notification email

--- administrator/connection/PDO_conn.php
+++ administrator/connection/PDO_conn.php
@@ -26,7 +26,7 @@ if ($server == 1) { //1= local, 2 = staging, 3 = live;
 //         email setting
 //-------------------------------------
 
-$admin_email = "notification@caroma.com.my";
+$admin_email = "sales@caroma.com.my";
 $email_host = "mail.caroma.com.my";
 $email_username = "test@caroma.com.my";
 $email_password = "=HV[GXQv+7l?";
