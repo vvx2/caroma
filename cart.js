a6774c3545fcbed6e34ceaaebbc7d5dd6b777172
puahvvx2 <puahweewee@gmail.com>
2021-06-11 00:21:07 +0800

    language part modify

--- cart.js
+++ cart.js
@@ -192,9 +192,9 @@ $(document).on('click', '.remove_cart', function () {
     RemoveFromCart(product_id, thisbtn, token);
 });
 
-$('[name="change_language"]').change(function () {
-    var language = $(this).val();
-
+$('[class="change_language"]').click(function () {
+    var language = $(this).data('value');
+    console.log("lang:" + language);
     $.post('api/change_language.php', {
         language: language
     }, function (data) {
