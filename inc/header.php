83965708ebe9b91bf932bd36becc67f2287a1ca8
Nicky <nickyz111406@gmail.com>
2021-06-10 23:57:00 +0800

    10-6-21 update 2

--- inc/header.php
+++ inc/header.php
@@ -11,7 +11,21 @@
             <li><a href="https://www.instagram.com/drinkcaroma/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
             <li><a href="https://www.facebook.com/caromamalaysia"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
         </ul>
-        <ul class="horizontal-menu">
+        <ul class="t1 horizontal-menu t11">
+            <li class="t2">
+                <div class="btn-group"> <a data-toggle="dropdown" type="button" class="btn btn-default" href="#"><img src="assets/images/en_US.png"></img> English</a>
+                    <ul class="dropdown-menu dml t1" role="menu">
+                        <li class="t2"><a href="#"><img src="assets/images/en_US.png"></img> English</a>
+                        </li>
+                        <li class="t2"><a href="#"><img src="assets/images/ms_MY.png"> Malayu</a>
+                        </li>
+                        <li class="t2"><a href="#"><img src="assets/images/zh_CN.png"> Chinese</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        <ul>
+        <!--- <ul class="horizontal-menu">
             <li class="horz-menu-item lang">
                 <select name="change_language">
                     <option value="en" <?php echo ($_SESSION['language'] == "en") ? "selected" : "" ?>>&nbsp;</option>
@@ -19,7 +33,7 @@
                     <option value="cn" <?php echo ($_SESSION['language'] == "cn") ? "selected" : "" ?>>&nbsp;</option>
                 </select>
             </li>
-        </ul>
+        </ul> --->
         <?php
         if ($login == 1) {
         ?>
@@ -32,3 +46,40 @@
         ?>
     </div>
 </div>
+
+<style>
+ul.t1 {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    float: left;
+}
+ul.t1 li.t2 {
+    display: inline-block;
+}
+.btn-group, .btn-group-vertical {
+    display: inline-block;
+    position: relative;
+    vertical-align: text-bottom;
+}
+.btn-group a.btn {
+    display: inline-block;
+    padding:0;
+    margin:0;
+    border-width: 0;
+    font-size: initial !important;
+    text-shadow: none !important;
+    background-color:inherit !important;
+    font-size: inherit !important;
+    line-height: initial !important;
+}
+
+.t11{
+    font-size : 15px !important;
+    padding: 5px 0px !important;
+}
+
+.dml{
+    min-width : 110px !important;
+}
+</style>
